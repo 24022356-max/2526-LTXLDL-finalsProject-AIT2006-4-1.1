acaa28ca5f17ef76f2a0dc39a2ff2466cafdb7d3
tmcng <tamanhcuong2006@gmail.com>
2025-12-12 01:20:25 +0700

    Fix duplicate rows in weekly

--- src/aggregate.py
+++ src/aggregate.py
@@ -18,18 +18,36 @@ YEAR = 2019
 # ------------------------------
 clean_files = sorted(glob.glob(os.path.join(INPUT_FOLDER, f'clean_yellow_tripdata_{YEAR}-*.parquet')))
 
-daily_chunk = []
-weekly_chunk = []
-monthly_chunk = []
-
-# payment_type, PULocationID and DOLocationID KPIs for monthly only
-monthly_chunk_pickup = []
-monthly_chunk_dropoff = []
-monthly_chunk_payment_type = []
+chunks = {
+    'daily': [],
+    'weekly': [],
+    'monthly': [],
+    'monthly_pickup': [],
+    'monthly_dropoff': [],
+    'monthly_payment_type': []
+}
 
 
 if not clean_files:
     raise FileNotFoundError('No cleaned parquet files found in processed/. Please run cleaning first.')
+
+# ------------------------------
+# AGGREGATE LOGIC
+# ------------------------------
+def apply_agg(df):
+    return df.agg(
+        trips=('VendorID', 'count'),
+        duration_p50=('trip_duration', 'median'),
+        duration_p95=('trip_duration', lambda x: x.quantile(0.95)),
+        speed_p50=('avg_speed', 'median'),
+        total_money=('total_amount', 'sum'),
+        passenger_p50=('passenger_count', 'median'),
+        passenger_mean=('passenger_count', 'mean'),
+        passenger_sum=('passenger_count', 'sum'),
+        distance_sum=('trip_distance', 'sum'),
+        distance_p50=('trip_distance', 'median'),
+        distance_mean=('trip_distance', 'mean'),
+    )
 
 for f in clean_files:
     print(f'Processing: {os.path.basename(f)}')
@@ -38,142 +56,24 @@ for f in clean_files:
     df['date'] = df['tpep_pickup_datetime'].dt.date
     df['year'] = df['tpep_pickup_datetime'].dt.year
     df['month'] = df['tpep_pickup_datetime'].dt.to_period('M')
-    df['week'] = df['tpep_pickup_datetime'].dt.isocalendar().week
-    df['dow'] = df['tpep_pickup_datetime'].dt.dayofweek   # Monday=0
+    df['week_start'] = df['tpep_pickup_datetime'].dt.to_period('W').dt.start_time
 
-    base_col = ['date', 'dow', 'week', 'month', 'year', 
+    base_col = ['date', 'month', 'year', 'week_start',
                 'VendorID', 'payment_type', 'PULocationID', 'DOLocationID',
                 'passenger_count', 'trip_distance', 'total_amount', 
                 'avg_speed', 'trip_duration']
     df_base = df[base_col].copy()
     
     # =====================================================
-    #   DAILY KPI
+    #  KPI
     # =====================================================
-    daily_chunk.append(
-    df_base.groupby(['date']).agg(
-        trips=('VendorID', 'count'),
-        duration_p50=('trip_duration', 'median'),
-        duration_p95=('trip_duration', lambda x: x.quantile(0.95)),
-        speed_p50=('avg_speed', 'median'),
-
-        total_money=('total_amount', 'sum'),
-
-        passenger_p50=('passenger_count', 'median'),
-        passenger_mean=('passenger_count', 'mean'),
-        passenger_sum=('passenger_count', 'sum'),
-
-        distance_sum=('trip_distance', 'sum'),
-        distance_p50=('trip_distance', 'median'),
-        distance_mean=('trip_distance', 'mean'),
-    )
-    )
-
-
-    #======================================================
-    # WEEKLY KPI
-    #======================================================
-
-    weekly_chunk.append(
-    df_base.groupby(['week']).agg(
-        trips=('VendorID', 'count'),
-        duration_p50=('trip_duration', 'median'),
-        duration_p95=('trip_duration', lambda x: x.quantile(0.95)),
-        speed_p50=('avg_speed', 'median'),
-
-        total_money=('total_amount', 'sum'),
-
-        passenger_p50=('passenger_count', 'median'),
-        passenger_mean=('passenger_count', 'mean'),
-        passenger_sum=('passenger_count', 'sum'),
-
-        distance_p50=('trip_distance', 'median'),
-        distance_mean=('trip_distance', 'mean'),
-        distance_sum=('trip_distance', 'sum'),
-    )
-    )
-
-    # =====================================================
-    #   MONTHLY KPI
-    # =====================================================
-    monthly_chunk.append(
-    df_base.groupby(['month']).agg(
-        trips=('VendorID', 'count'),
-        duration_p50=('trip_duration', 'median'),
-        duration_p95=('trip_duration', lambda x: x.quantile(0.95)),
-        speed_p50=('avg_speed', 'median'),
-
-        total_money=('total_amount', 'sum'),
-
-        passenger_p50=('passenger_count', 'median'),
-        passenger_mean=('passenger_count', 'mean'),
-        passenger_sum=('passenger_count', 'sum'),
-
-        distance_sum=('trip_distance', 'sum'),
-        distance_p50=('trip_distance', 'median'),
-        distance_mean=('trip_distance', 'mean'),
-    )
-    )
-
-    monthly_chunk_pickup.append(
-    df_base.groupby(['month', 'PULocationID']).agg(
-        trips=('VendorID', 'count'),
-        duration_p50=('trip_duration', 'median'),
-        duration_p95=('trip_duration', lambda x: x.quantile(0.95)),
-        speed_p50=('avg_speed', 'median'),
-
-        total_money=('total_amount', 'sum'),
-
-        passenger_p50=('passenger_count', 'median'),
-        passenger_mean=('passenger_count', 'mean'),
-        passenger_sum=('passenger_count', 'sum'),
-
-        distance_sum=('trip_distance', 'sum'),
-        distance_p50=('trip_distance', 'median'),
-        distance_mean=('trip_distance', 'mean'),
-    )
-    )
-
-
-    monthly_chunk_dropoff.append(
-    df_base.groupby(['month', 'DOLocationID']).agg(
-        trips=('VendorID', 'count'),
-        duration_p50=('trip_duration', 'median'),
-        duration_p95=('trip_duration', lambda x: x.quantile(0.95)),
-        speed_p50=('avg_speed', 'median'),
-
-        total_money=('total_amount', 'sum'),
-
-        passenger_p50=('passenger_count', 'median'),
-        passenger_mean=('passenger_count', 'mean'),
-        passenger_sum=('passenger_count', 'sum'),
-
-        distance_sum=('trip_distance', 'sum'),
-        distance_p50=('trip_distance', 'median'),
-        distance_mean=('trip_distance', 'mean'),
-    )
-    )
-
-    monthly_chunk_payment_type.append(
-    df_base.groupby(['month', 'payment_type']).agg(
-        trips=('VendorID', 'count'),
-        duration_p50=('trip_duration', 'median'),
-        duration_p95=('trip_duration', lambda x: x.quantile(0.95)),
-        speed_p50=('avg_speed', 'median'),
-
-        total_money=('total_amount', 'sum'),
-
-        passenger_p50=('passenger_count', 'median'),
-        passenger_mean=('passenger_count', 'mean'),
-        passenger_sum=('passenger_count', 'sum'),
-
-        distance_sum=('trip_distance', 'sum'),
-        distance_p50=('trip_distance', 'median'),
-        distance_mean=('trip_distance', 'mean'),
-    )
-    )
-
-
+    chunks['daily'].append(apply_agg(df.groupby(['date'])))
+    chunks['weekly'].append(apply_agg(df.groupby('week_start')))
+    chunks['monthly'].append(apply_agg(df.groupby(['month'])))
+    
+    chunks['monthly_pickup'].append(apply_agg(df.groupby(['month', 'PULocationID'])))
+    chunks['monthly_dropoff'].append(apply_agg(df.groupby(['month', 'DOLocationID'])))
+    chunks['monthly_payment_type'].append(apply_agg(df.groupby(['month', 'payment_type'])))
 
     del df, df_base
     gc.collect()
@@ -183,53 +83,54 @@ for f in clean_files:
 #   CHUNK MERGE
 #-------------------------------
 
-kpi_daily = pd.concat(daily_chunk).reset_index()
-kpi_weekly = pd.concat(weekly_chunk).reset_index()
-kpi_monthly = pd.concat(monthly_chunk).reset_index()
+kpi_daily = pd.concat(chunks['daily']).reset_index()
+kpi_monthly = pd.concat(chunks['monthly']).reset_index()
 
-kpi_monthly_pickup = pd.concat(monthly_chunk_pickup).reset_index()
-kpi_monthly_dropoff = pd.concat(monthly_chunk_dropoff).reset_index()
-kpi_monthly_payment_type = pd.concat(monthly_chunk_payment_type).reset_index()
+kpi_monthly_pickup = pd.concat(chunks['monthly_pickup']).reset_index()
+kpi_monthly_dropoff = pd.concat(chunks['monthly_dropoff']).reset_index()
+kpi_monthly_payment_type = pd.concat(chunks['monthly_payment_type']).reset_index()
 
-# =====================================================
-#   COMPUTE % OF YEAR TRIPS AND MONEY
-# =====================================================
-total_trips = kpi_monthly['trips'].sum()
+# ------------------------------
+# Fix duplicate rows in weekly
+# ------------------------------
 
-kpi_daily['trip_pct'] = 100 * kpi_daily['trips'] / total_trips
-kpi_weekly['trip_pct'] = 100 * kpi_weekly['trips'] / total_trips
-kpi_monthly['trip_pct'] = 100 * kpi_monthly['trips'] / total_trips
-
-kpi_monthly_payment_type['trip_pct'] = 100 * kpi_monthly_payment_type['trips'] / total_trips
-kpi_monthly_pickup['trip_pct'] = 100 * kpi_monthly_pickup['trips'] / total_trips
-kpi_monthly_dropoff['trip_pct'] = 100 * kpi_monthly_dropoff['trips'] / total_trips
-
-total_money = kpi_monthly['total_money'].sum()
-
-kpi_daily['money_pct'] = 100 * kpi_daily['total_money'] / total_money
-kpi_weekly['money_pct'] = 100 * kpi_weekly['total_money'] / total_money
-kpi_monthly['money_pct'] = 100 * kpi_monthly['total_money'] / total_money
-
-kpi_monthly_payment_type['money_pct'] = 100 * kpi_monthly_payment_type['total_money'] / total_money
-kpi_monthly_pickup['money_pct'] = 100 * kpi_monthly_pickup['total_money'] / total_money
-kpi_monthly_dropoff['money_pct'] = 100 * kpi_monthly_dropoff['total_money'] / total_money
-
+kpi_weekly = (pd.concat(chunks['weekly']).reset_index()).groupby('week_start').agg(
+    trips=('trips', 'sum'),
+    total_money=('total_money', 'sum'),
+    passenger_sum=('passenger_sum', 'sum'),
+    distance_sum=('distance_sum', 'sum'),
+    duration_p50=('duration_p50', 'mean'), 
+    duration_p95=('duration_p95', 'mean'),
+    speed_p50=('speed_p50', 'mean'),
+    passenger_p50=('passenger_p50', 'mean'),
+    distance_p50=('distance_p50', 'mean'),
+).reset_index()
+kpi_weekly['passenger_mean'] = kpi_weekly['passenger_sum'] / kpi_weekly['trips']
+kpi_weekly['distance_mean'] = kpi_weekly['distance_sum'] / kpi_weekly['trips']
+kpi_weekly = kpi_weekly.sort_values('week_start')
 
 # =====================================================
-#   SAVE OUTPUTS
+#   COMPUTE PERCENTAGE AND SAVE OUTPUTS
 # =====================================================
-daily_path  = os.path.join(OUTPUT_FOLDER, f'kpi_daily_{YEAR}.csv')
-weekly_path = os.path.join(OUTPUT_FOLDER, f'kpi_weekly_{YEAR}.csv')
-monthly_path = os.path.join(OUTPUT_FOLDER, f'kpi_monthly_{YEAR}.csv')
+output_path = {
+    'daily': f'kpi_daily_{YEAR}.csv',
+    'weekly': f'kpi_weekly_{YEAR}.csv',
+    'monthly': f'kpi_monthly_{YEAR}.csv',
+    'monthly_pickup': f'kpi_monthly_pickup_{YEAR}.csv',
+    'monthly_dropoff': f'kpi_monthly_dropoff_{YEAR}.csv',
+    'monthly_payment_type': f'kpi_monthly_payment_type_{YEAR}.csv'
+}
 
-monthly_pt_path = os.path.join(OUTPUT_FOLDER, f'kpi_monthly_payment_type_{YEAR}.csv')
-monthly_pu_path = os.path.join(OUTPUT_FOLDER, f'kpi_monthly_pickup_{YEAR}.csv')
-monthly_do_path = os.path.join(OUTPUT_FOLDER, f'kpi_monthly_dropoff_{YEAR}.csv')
+temp_monthly = pd.concat(chunks['monthly'])
+total_trips_year = temp_monthly['trips'].sum()
+total_money_year = temp_monthly['total_money'].sum()
 
-kpi_daily.to_csv(daily_path, index=False)
-kpi_weekly.to_csv(weekly_path, index=False)
-kpi_monthly.to_csv(monthly_path, index=False)
+for key, filename in output_path.items():
+    df_final = pd.concat(chunks[key]).reset_index()
+    
+    # Calculate Percentages
+    df_final['trip_pct'] = (df_final['trips'] / total_trips_year) * 100
+    df_final['money_pct'] = (df_final['total_money'] / total_money_year) * 100
 
-kpi_monthly_pickup.to_csv(monthly_pu_path, index=False)
-kpi_monthly_dropoff.to_csv(monthly_do_path, index=False)
-kpi_monthly_payment_type.to_csv(monthly_pt_path, index=False)
+    df_final.to_csv(os.path.join(OUTPUT_FOLDER, filename), index=False)
+    print(f"Saved: {filename}")
